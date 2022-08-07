098c35ddef0e4177f37919b0a1b88f8e779d391a
ig0rN <igorn995@gmail.com>
2022-08-07 21:49:32 +0200

    all possible action in one controller method

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -2,12 +2,9 @@
 
 namespace App\Controller;
 
-use App\Feature\Ticket\Action\CancelAction;
-use App\Feature\Ticket\Action\ChangeSeatAction;
-use App\Feature\Ticket\Action\CreateAction;
-use App\Feature\Ticket\DTO\ChangeSeatRequest;
-use App\Feature\Ticket\DTO\CreateRequest;
-use App\Service\RequestDTOFactory;
+use App\Feature\Ticket\Service\ActionHandler;
+use App\Feature\Ticket\Service\ActionRequestModelFactory;
+use App\Feature\Ticket\Service\RequestValidator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,52 +17,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class TicketController
 {
     #[Route(
-        name: 'create',
+        path: '/{action}',
         methods: [Request::METHOD_POST]
     )]
-    public function create(
+    public function handle(
         Request $request,
-        RequestDTOFactory $requestDTOFactory,
-        CreateAction $createAction,
+        string $action,
+        RequestValidator $requestValidator,
+        ActionRequestModelFactory $requestModelFactory,
+        ActionHandler $actionHandler,
     ): JsonResponse {
-        $dto = $requestDTOFactory->make($request, CreateRequest::class);
+        $requestValidator->validateRequest($request, $action);
+
+        $dto = $requestModelFactory->make($request, $action);
 
         return new JsonResponse(
-            $createAction->handleRequest($dto),
-            Response::HTTP_CREATED
-        );
-    }
-
-    #[Route(
-        path: '/{uuid}/cancel',
-        name: 'cancel',
-        methods: [Request::METHOD_GET]
-    )]
-    public function cancel(
-        string $uuid,
-        CancelAction $cancelAction,
-    ): JsonResponse {
-        return new JsonResponse(
-            $cancelAction->handleRequest($uuid),
-            Response::HTTP_OK
-        );
-    }
-
-    #[Route(
-        path: '/{uuid}/change-seat',
-        name: 'change-seat',
-        methods: [Request::METHOD_PUT]
-    )]
-    public function changeSeat(
-        Request $request,
-        string $uuid,
-        RequestDTOFactory $requestDTOFactory,
-        ChangeSeatAction $changeSeatAction,
-    ): JsonResponse {
-        $dto = $requestDTOFactory->make($request, ChangeSeatRequest::class);
-
-        return new JsonResponse(
-            $changeSeatAction->handleRequest($uuid, $dto),
+            $actionHandler->getResponseFromAction($dto),
             Response::HTTP_OK
         );
     }
